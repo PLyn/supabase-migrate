7e76dff649f294208439e16ae055c09658d890ab
PLyn <plyn8750@gmail.com>
2025-07-01 13:08:44 -0500

    rip out tailwind and daisy ui. Setup barebones test for migrate page

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -12,13 +12,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	},
-
-	server: {
-		proxy: {
-			'/connect-supabase': 'http://localhost:3000',
-			'/api': 'http://localhost:3000'
-		}
 	}
 };
 
